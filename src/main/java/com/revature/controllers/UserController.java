3fa770a8ad730f4cbd6212f2a2bbd7b03f6a677d
Stephen Yang <stephenyang@creighton.edu>
2018-11-29 09:16:05 -0500

    login with POST

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,12 +1,12 @@
 package com.revature.controllers;
 
-import javax.servlet.http.HttpSession;
-
 import org.apache.log4j.Logger;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.daos.UserDaoImpl;
@@ -15,6 +15,7 @@ import com.revature.models.User;
 //@Controller
 @RestController// same as Controller but w/o ResponseBody?
 //@RequestMapping("/users")
+@CrossOrigin
 public class UserController {
 	
 	private final static Logger ucLog = Logger.getLogger(UserController.class);
@@ -27,8 +28,20 @@ public class UserController {
 	
 	@GetMapping(value="/login", produces = MediaType.APPLICATION_JSON_VALUE)
 	//@ResponseBody
-	public User login(@RequestParam("username") String username, @RequestParam("password") String password) {
+	public User loginGet(@RequestParam("username") String username, @RequestParam("password") String password) {
 		User login = UserDaoImpl.getDao().getUser(username, password);
+		if (login != null) {
+			ucLog.info("login successful");
+		} else {
+			ucLog.info("login failed");
+		}
+		return login;
+	}
+	
+	@PostMapping(value="/login", produces = MediaType.APPLICATION_JSON_VALUE)
+	public User login(@RequestBody String formStr) { // username and password as one String
+		String[] formArr = formStr.split(" ");
+		User login = UserDaoImpl.getDao().getUser(formArr[0], formArr[1]);
 		if (login != null) {
 			ucLog.info("login successful");
 		} else {
